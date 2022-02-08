d449b4abe32b21a60e044bb0fe0c44531d11b3c5
r-achanova <achanova@abv.bg>
2022-02-08 12:23:20 +0200

    Upload Project

---
+++ Arrays/ObrabotkaMasiv/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ObrabotkaMasiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command=="Reverse")
+                {
+                    input = input.Reverse().ToArray();
+                }
+                else if (command=="Distinct")
+                {
+                    input = input.Distinct().ToArray();
+                }
+                else if (command=="Replace") //Replace 2 Hello
+                {
+                    int index = int.Parse(cmd[1]);
+                    string strForReplace = cmd[2];
+                    input[index] = strForReplace;
+                }
+            }
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
